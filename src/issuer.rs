64277e4446f97570848fda355de4f939b11f60b3
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-03-06 14:08:27 +0900

    Update : cargo fmt

--- src/issuer.rs
+++ src/issuer.rs
@@ -28,7 +28,8 @@ mod tests {
                 hasher.update(value_str.as_bytes());
                 let hashed = hasher.finalize();
                 // 바이트 배열을 16진수 문자열로 변환
-                let hashed_decimal_str = hashed.iter()
+                let hashed_decimal_str = hashed
+                    .iter()
                     .flat_map(|&byte| format!("{}", byte).chars().collect::<Vec<_>>()) // 각 바이트를 10진수 문자열로 변환하고, 문자들을 flat_map을 사용해 하나의 이터레이터로 만듭니다.
                     .filter(|&c| c.is_digit(10)) // 0부터 9까지의 숫자인지 확인합니다.
                     .take(38) // 최초 38개의 문자만 선택합니다.
@@ -53,7 +54,8 @@ mod tests {
                         let mut hasher = Sha256::new();
                         hasher.update(value_str.as_bytes());
                         let hashed = hasher.finalize();
-                        let hashed_decimal_str = hashed.iter()
+                        let hashed_decimal_str = hashed
+                            .iter()
                             .flat_map(|&byte| format!("{}", byte).chars().collect::<Vec<_>>()) // 각 바이트를 10진수 문자열로 변환하고, 문자들을 flat_map을 사용해 하나의 이터레이터로 만듭니다.
                             .filter(|&c| c.is_digit(10)) // 0부터 9까지의 숫자인지 확인합니다.
                             .take(38) // 최초 38개의 문자만 선택합니다.
@@ -78,27 +80,27 @@ mod tests {
         let json_hashes_pretty = to_string_pretty(&hashes).expect("Failed to serialize hashes");
 
         // 파일에 JSON 데이터 쓰기
-        let mut file = File::create("./zok/issuer/memo.json").expect("Failed to create file");
+        let mut file = File::create("./zok/issuer/credential.json").expect("Failed to create file");
         file.write_all(json_hashes_pretty.as_bytes())
             .expect("Failed to write to file");
     }
 
     fn load_credential_hash() -> Vec<String> {
         // 파일로부터 JSON 데이터 읽기
-        let data = fs::read_to_string("./zok/issuer/memo.json").expect("Unable to read file");
+        let data = fs::read_to_string("./zok/issuer/credential.json").expect("Unable to read file");
 
         // 읽은 데이터를 JSON으로 파싱
         let hashes: Map<String, Value> = serde_json::from_str(&data).expect("Unable to parse JSON");
 
         // JSON 객체에서 value 값만 추출하여 Vec<String>에 담아 리턴
         hashes
-        .iter()
-        .filter_map(|(_, value)| match value {
-            Value::String(val) => Some(val.clone()), // 값이 문자열인 경우
-            Value::Number(num) => Some(num.to_string()), // 값이 숫자인 경우, 숫자를 문자열로 변환
-            _ => None // 그 외의 경우는 None을 반환하여 filter_map에서 제외
-        })
-        .collect()
+            .iter()
+            .filter_map(|(_, value)| match value {
+                Value::String(val) => Some(val.clone()), // 값이 문자열인 경우
+                Value::Number(num) => Some(num.to_string()), // 값이 숫자인 경우, 숫자를 문자열로 변환
+                _ => None, // 그 외의 경우는 None을 반환하여 filter_map에서 제외
+            })
+            .collect()
     }
 
     use std::process::Command;
@@ -121,6 +123,8 @@ mod tests {
         args.extend(credential_hash_param.into_iter());
         args.push("--verbose".into());
         args.push("--json".into());
+
+        print!("args : {:?}", args);
 
         let compute_witness_status = Command::new("zokrates")
             .current_dir("./zok/issuer") // 작업 디렉토리 설정
@@ -158,22 +162,36 @@ mod tests {
         let witness_values = load_zokrates_witness();
 
         // witness 값 JSON 파일로 저장 (예: witness_values.json)
-        let witness_file_path = "witness_values.json";
+        let witness_file_path = "./zok/issuer/witness_values.json";
         let mut file = File::create(witness_file_path).expect("Unable to create witness file");
-        let witness_json = serde_json::to_string(&witness_values).expect("Unable to serialize witness values");
+        let witness_json =
+            serde_json::to_string(&witness_values).expect("Unable to serialize witness values");
         writeln!(file, "{}", witness_json).expect("Unable to write witness values");
-        
+
         // Python 스크립트 실행
-        // signature 및 public key 생성 스크립트.
+        // signature 및 publi key 생성 스크립트.
         // 현재는 기존 조크라테스 크립토를 그대로 사용하나, 추후 조크라테스 크립토를 RustZoCrypto로 마이그레이션 후 대체할 예정.
         let output = Command::new("python3")
             .current_dir("./zok/issuer")
-            .arg("./create_signature.py")
-            .arg(witness_file_path) // 파이썬 스크립트에 witness 파일 경로를 인자로 전달
+            .arg("create_signature.py")
             .output()
             .expect("Failed to execute python script");
 
         // 실행 성공 여부 확인
         assert!(output.status.success(), "Python script execution failed");
+    }
+
+    // 파일 이동을 위한 함수
+    fn move_file_to_prover(file_name: &str) {
+        let source_path = format!("./zok/issuer/{}", file_name);
+        let destination_path = format!("./zok/prover/{}", file_name);
+
+        fs::rename(&source_path, &destination_path)
+            .expect(&format!("Failed to move {} to ./zok/prover", file_name));
+    }
+    #[test]
+    fn Issuance_to_prover() {
+        move_file_to_prover("signature");
+        move_file_to_prover("credential.json");
     }
 }
--- src/verifier.rs
+++ src/verifier.rs
@@ -1,6 +1,17 @@
 #[cfg(test)]
 mod tests {
+    use std::fs;
     use std::process::Command;
+
+    fn move_proving_key() {
+        // `proving.key` 파일의 현재 경로
+        let source_path = "./zok/verifier/proving.key";
+        // `proving.key` 파일을 이동할 목적지 경로
+        let destination_path = "./zok/prover/proving.key";
+
+        // 파일 이동
+        fs::rename(source_path, destination_path).expect("Failed to move proving.key file");
+    }
 
     #[test]
     fn zokrates_setup() {
@@ -15,12 +26,15 @@ mod tests {
         assert!(compile_status.success()); // 컴파일 성공 확인
 
         // `zokrates setup` 명령어 실행
-         let compile_status = Command::new("zokrates")
-         .current_dir("./zok/verifier") // 작업 디렉토리 설정
-         .arg("setup")
-         .status() // 명령어 실행
-         .expect("Failed to execute zokrates setup");
+        let compile_status = Command::new("zokrates")
+            .current_dir("./zok/verifier") // 작업 디렉토리 설정
+            .arg("setup")
+            .status() // 명령어 실행
+            .expect("Failed to execute zokrates setup");
         assert!(compile_status.success()); // setup 성공 확인
+
+        // `proving.key` 파일을 prover에게 전달
+        move_proving_key();
     }
 
     #[test]
@@ -28,10 +42,10 @@ mod tests {
         // `zokrates export-verifier` 명령어 실행
         // verifier.sol 컨트렉트 생성
         let compile_status = Command::new("zokrates")
-        .current_dir("./zok/verifier") // 작업 디렉토리 설정
-        .arg("export-verifier")
-        .status() // 명령어 실행
-        .expect("Failed to execute zokrates setup");
-       assert!(compile_status.success()); // setup 성공 확인
+            .current_dir("./zok/verifier") // 작업 디렉토리 설정
+            .arg("export-verifier")
+            .status() // 명령어 실행
+            .expect("Failed to execute zokrates setup");
+        assert!(compile_status.success()); // setup 성공 확인
     }
 }
